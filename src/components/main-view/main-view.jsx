bcd48aac03c45df253ce2c0c2f95e64852c9f991
Jorge Cortés <jorge.cortes@gmail.com>
2024-04-19 10:44:39 -0700

    it fetch movies from API and add propTypes

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,60 +1,61 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
-  const [movies, setMovies] = useState([
-    {
-      id: 1,
-      title: "Fight Club",
-      description: "An insomniac office worker and a devil-may-care soap maker form an underground fight club that evolves...",
-      genre: { name: "Drama" },
-      director: { name: "David Fincher" },
-      image:
-        "https://upload.wikimedia.org/wikipedia/en/f/fc/Fight_Club_poster.jpg",
-    },
-    {
-      id: 2,
-      title: "Schindler's List",
-      description: "In German-occupied Poland during World War II, industrialist Oskar Schindler gradually becomes concerned...",
-      genre: { name: "Biography" },
-      director: { name: "Steven Spielberg" },
-      image:
-        "https://upload.wikimedia.org/wikipedia/en/3/38/Schindler%27s_List_movie.jpg",
-    },
-    {
-      id: 3,
-      title: "The Shawshank Redemption",
-      description: "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common...",
-      genre: { name: "Drama" },
-      director: { name: "Frank Darabont" },
-      image:
-        "https://upload.wikimedia.org/wikipedia/en/8/81/ShawshankRedemptionMoviePoster.jpg",
-    },
-    {
-      id: 4,
-      title: "The Dark Knight",
-      description: "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept...",
-      genre: { name: "Action" },
-      director: { name: "Christopher Nolan" },
-      image:
-        "https://upload.wikimedia.org/wikipedia/en/1/1c/The_Dark_Knight_%282008_film%29.jpg",
-    },
-    {
-      id: 5,
-      title: "The Matrix",
-      description: "A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war...",
-      genre: { name: "Sci-Fi" },
-      director: { name: "Lana Wachowski" },
-      image:
-        "https://upload.wikimedia.org/wikipedia/en/c/c1/The_Matrix_Poster.jpg",
-    }
-  ]);
-
+  const [movies, setMovies] = useState([]);
   const [selectedMovie, setSelectedMovie] = useState(null);
 
+  useEffect(() => {
+    fetch("https://cf-2-movie-api.onrender.com/movies")
+      .then((response) => response.json())
+      .then(({ data }) => {
+        // Please review the response format of the API here
+        // https://cf-2-movie-api.onrender.com/docs/#/Movie/get_movies
+        const moviesFromApi = data.map(movie => {
+          return {
+            id: movie._id,
+            Title: movie.Title,
+            Description: movie.Description,
+            ImagePath: movie.ImagePath,
+            Featured: movie.Featured,
+            ReleaseYear: movie.ReleaseYear,
+            MPA: movie.MPA,
+            IMDb: movie.IMDb,
+            Genre: movie.Genre,
+            Director: movie.Director,
+            Actors: movie.Actors,
+          }
+        });
+
+        setMovies(moviesFromApi);
+      });
+  }, []);
+
   if (selectedMovie) {
-    return <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
+    // Bonus Task 2: Similar Movies
+    const similarMovies = movies.filter(movie => {
+      const isSimilarMovie = movie.Genre.Name === selectedMovie.Genre.Name;
+      const isNotTheSelectedMovie = movie.id !== selectedMovie.id;
+      return isNotTheSelectedMovie && isSimilarMovie;
+    });
+
+    return (
+      <>
+        <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
+        <hr />
+        <h2>Similar Movies</h2>
+        {similarMovies.map(movie => (
+          <MovieCard
+            key={movie.id}
+            movie={movie}
+            onMovieClick={(newSelectedMovie) => {
+              setSelectedMovie(newSelectedMovie);
+            }}
+          />
+        ))}
+      </>
+    );
   }
 
   if (movies.length === 0) {
